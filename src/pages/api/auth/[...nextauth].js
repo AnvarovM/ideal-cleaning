efbdf2919b6a24932f2efb61e999831cea433536
Anvarovm <anvarovmuxammadxuja2001@gmail.com>
2021-12-29 14:14:20 +0500

    pushed

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -5,8 +5,8 @@ export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     Providers.Google({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
+      clientId: '496560778128-cgdhg5dlpurqnds8883oqslkabatmst0.apps.googleusercontent.com',
+      clientSecret: "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",
     }),
     // Providers.Email({
     //   clientId: process.env.GOOGLE_ID,
